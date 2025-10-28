8509483aa58714b4efc9c6e7dbd85fe0caa22aaf
Kamil Topolewski <kamil.topolewski@skyscanner.net>
2025-10-28 07:21:29 +0000

    Add reservation db schema.

--- cars-service/src/main/java/com/carrental/cars_service/CarsServiceApplication.java
+++ cars-service/src/main/java/com/carrental/cars_service/CarsServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarsServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CarsServiceApplication.class, args);
+  }
 }
--- reservation-service/src/main/java/com/carrental/reservation_service/model/Reservation.java
+++ reservation-service/src/main/java/com/carrental/reservation_service/model/Reservation.java
@@ -1,4 +1,46 @@
 package com.carrental.reservation_service.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reservations")
+@Data
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String customerId;
+
+    @Column(nullable = false)
+    private String carId;
+
+    @Column(nullable = false)
+    private LocalDateTime reservationStartDate;
+
+    @Column(nullable = false)
+    private Integer duration;
+
+    @Column(nullable = false)
+    @Positive
+    private Float price;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
